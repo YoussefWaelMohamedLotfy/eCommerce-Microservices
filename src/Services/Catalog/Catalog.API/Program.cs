79e33ae7fbf4509efd3fa64a9637a5360fc3a8b1
Yousef Wael <youssefwael.mohamedlotfy@se.com>
2023-10-16 21:25:50 +0300

    Added Logic for Catalog API

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,4 +1,10 @@
+using Catalog.API.Data;
+using Catalog.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<ICatalogContext, CatalogContext>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -16,29 +22,47 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+var catalogEndpointGroup = app.MapGroup("/api/v1/Catalog")
+    .WithOpenApi();
 
-app.MapGet("/weatherforecast", () =>
+catalogEndpointGroup.MapGet("/", async (IProductRepository repo) =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    var products = await repo.GetProducts().ConfigureAwait(false);
+    return Results.Ok(products);
 })
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    .WithSummary("Get all products");
+
+catalogEndpointGroup.MapGet("/{id:length(24)}", async (string id, IProductRepository repo) =>
+{
+    var product = await repo.GetProduct(id).ConfigureAwait(false);
+    return Results.Ok(product);
+})
+    .WithName("GetProduct")
+    .WithSummary("Get product with an ID");
+
+catalogEndpointGroup.MapGet("/Category/{category}", async (string category, IProductRepository repo) =>
+{
+    var products = await repo.GetProductByCategory(category).ConfigureAwait(false);
+    return Results.Ok(products);
+})
+    .WithSummary("Get products for a category");
+
+catalogEndpointGroup.MapPost("/", async (Product newProduct, IProductRepository repo) =>
+{
+    await repo.CreateProduct(newProduct).ConfigureAwait(false);
+    return Results.CreatedAtRoute("GetProduct", new { id = newProduct.Id }, newProduct);
+})
+    .WithSummary("Creates a new product in catalog");
+
+catalogEndpointGroup.MapPut("/", async (Product updatedProduct, IProductRepository repo)
+    => Results.Ok(await repo.UpdateProduct(updatedProduct).ConfigureAwait(false)))
+    .WithSummary("Updates existing product in catalog");
+
+catalogEndpointGroup.MapDelete("/{id:length(24)}", async (string id, IProductRepository repo) =>
+{
+    var isDeleted = await repo.DeleteProduct(id).ConfigureAwait(false);
+    return isDeleted ? Results.NoContent() : Results.NotFound();
+})
+    .WithSummary("Deletes a product from catalog");
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
