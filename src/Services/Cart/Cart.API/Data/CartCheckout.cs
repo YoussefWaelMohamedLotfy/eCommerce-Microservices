dcdcd612fa081ffc9d0fafabd24b314f1fbd96ed
Youssef Wael Mohamed-Lotfy <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2023-10-17 10:55:32 +0300

    Add basic logic for Cart API (#3)
    * Add new Project for Cart.API
    
    * Added Logic for Cart API
    
    ---------
    
    Co-authored-by: Yousef Wael <youssefwael.mohamedlotfy@se.com>

---
+++ src/Services/Cart/Cart.API/Data/CartCheckout.cs
@@ -0,0 +1,34 @@
+﻿namespace Cart.API.Data;
+
+public sealed class CartCheckout
+{
+    public string UserName { get; set; } = default!;
+
+    public decimal TotalPrice { get; set; }
+
+    // BillingAddress
+    public string FirstName { get; set; } = default!;
+
+    public string LastName { get; set; } = default!;
+
+    public string EmailAddress { get; set; } = default!;
+
+    public string AddressLine { get; set; } = default!;
+
+    public string Country { get; set; } = default!;
+
+    public string State { get; set; } = default!;
+
+    public string ZipCode { get; set; } = default!;
+
+    // Payment
+    public string CardName { get; set; } = default!;
+
+    public string CardNumber { get; set; } = default!;
+
+    public string Expiration { get; set; } = default!;
+
+    public string CVV { get; set; } = default!;
+
+    public int PaymentMethod { get; set; }
+}
---
+++ src/Services/Cart/Cart.API/Data/ShoppingCart.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Cart.API.Data;
+
+[JsonSerializable(typeof(ShoppingCart))]
+public sealed partial class ShoppingCartContext : JsonSerializerContext { }
+
+public sealed class ShoppingCart
+{
+    public string? UserName { get; set; }
+
+    public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+    public ShoppingCart()
+    {
+    }
+
+    public ShoppingCart(string username)
+    {
+        UserName = username;
+    }
+
+    public decimal TotalPrice
+    {
+        get
+        {
+            decimal result = 0;
+
+            foreach (var item in Items)
+            {
+                result += item.Price * item.Quantity;
+            }
+
+            return result;
+        }
+    }
+}
