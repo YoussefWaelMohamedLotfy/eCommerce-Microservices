eb3ab1217bc48ce515a55257854226ae9be99ee8
Yousef Wael <youssefwael.mohamedlotfy@se.com>
2023-10-24 00:15:18 +0300

    Add new Project for YARP reverse proxy

---
+++ src/Gateways/YARP.Proxy/Program.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Serilog;
+using Shared.Utilites.HealthChecks;
+using Yarp.ReverseProxy.Transforms;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog(Serilogger.Configure);
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ConfigureEndpointDefaults(o => o.Protocols = HttpProtocols.Http1AndHttp2AndHttp3);
+    options.ConfigureHttpsDefaults(o => o.AllowAnyClientCertificate());
+});
+
+builder.Services.AddHealthChecks();
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
+        .AddTransforms(transforms =>
+        {
+            transforms.AddResponseTransform(transform =>
+            {
+                var testvalue = "Test-Value";
+                transform.ProxyResponse.Headers.Add("X-YARP-Response-Id", testvalue);
+                return ValueTask.CompletedTask;
+            });
+
+            transforms.AddRequestTransform(transform =>
+            {
+                var testvalue = "Test-Value";
+                transform.ProxyRequest.Headers.Add("X-YARP-Request-Id", testvalue);
+                return ValueTask.CompletedTask;
+            });
+        });
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello YARP!");
+app.MapCustomHealthChecks();
+app.MapReverseProxy();
+
+app.Run();
